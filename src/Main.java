041ddafa47dbaf10c2daa3719dedf26c4c20be74
zhangyanru <yuhezhangyanru@sina.com>
2019-07-10 12:59:31 +0800

    add test

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName Main
+ * @Decription TODO
+ * @Author Yanru Zhang
+ * @Date 2019/7/10 12:56
+ * @Version 1.0
+ **/
+public class Main {
+    public static void main(String [] args)
+    {
+        System.out.println("yanruTODO hello");
+    }
+}
